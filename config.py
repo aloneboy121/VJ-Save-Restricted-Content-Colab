b1c435e1a228d691006d5bdf5c68a09af24a85ec
Aloneboy <71880160+aloneboy121@users.noreply.github.com>
2024-07-07 19:59:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 #Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7106446721:AAFALLoVddnIKkNEWxAIk")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7106446721:AAFALLoVddnIKkNb-4Cazhz6ZsNuuEWxAIk")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "23480585"))
+API_ID = int(os.environ.get("API_ID", "23480585")
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "23af64c0a4ba8c1f0c04e9be07d0ba68)
+API_HASH = os.environ.get("API_HASH", "23af64c0a4ba8c1f0c04e9be07d0ba68")
 
 #Database 
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://prakashsalvi2121:4MPlXdXsvSYCyQ0o@vjsavebot.yosbnzp.mongodb.net/?retryWrites=true&w=majority&appName=Vjsavebot")
